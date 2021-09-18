a76d993ecb91752df6c7ef6dadf62edbc9f82692
Joan Lorenzo Matuto <matuto1820382@mkt.ceu.edu.ph>
2021-09-18 09:53:28 +0800

    Upload project

---
+++ resources/views/students/Create.blade.php
@@ -0,0 +1,57 @@
+@extends('students.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Students</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{route('students.index')}}">Back</a>
+        </div>
+    </div>
+</div>
+
+@if($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoopsie! </strong>There were some problems with your input. <br><br>
+        <ul>
+            @foreach($errors->all as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{route('students.store')}}" method="POST">
+    @csrf
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>StudentName:</strong>
+                <input type="text" name="studentname" class="form-control" placeholder="studentname">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Course:</strong>
+                <input type="text" name="course" class="form-control" placeholder="course">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Age:</strong>
+                <input type="text" name="age" class="form-control" placeholder="age">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
